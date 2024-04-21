10688b328cb7d4cf389bf9cc1e282053cf5a50db
spacepacket1 <104313479+spacepacket1@users.noreply.github.com>
2024-04-20 19:14:58 -0700

    Update E3DToken.sol
    Source code the the E3Dtoken

--- E3DToken.sol
+++ E3DToken.sol
@@ -1,1 +1,18 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract E3DToken is ERC20, ERC20Burnable, ERC20Permit, Ownable {
+    constructor(address initialOwner)
+        ERC20("E3DToken", "E3D")
+        ERC20Permit("E3DToken")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, 1000000 * 10 ** decimals()); // Initial supply: 1,000,000 E3D
+    }
+}
